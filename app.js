ea967a1e74e68fe8bb21fd7233b68469ad2cfe9f
Adam Miholek <Adam.Miholek@sosholic.sk>
2023-10-25 13:32:00 +0200

    uloha

--- app.js
+++ app.js
@@ -7,13 +7,34 @@
 
 //vysledok(vysledokScitania);
 
-let prvaPremenna = 3;
-let druhaPremenna = 7;
-let vysledokScitania = prvaPremenna + druhaPremenna;
-vysledok = vysledokScitania;
-alert("3+7");
-alert(3 + 7);
+//let prvaPremenna = 3;
+//let druhaPremenna = 7;
+//let vysledokScitania = prvaPremenna + druhaPremenna;
+//vysledok = vysledokScitania;
+//alert("3+7");
+//alert(3 + 7);
+//alert();
+//alert(prvaPremenna + druhaPremenna);
+//alert();
+//alert(vysledok);
+
+let prvaPremenna = 15;
+let druhaPremenna = 3;
+let tretiaPremenna = 4;
+let stvrtaPremenna = 8 / 2;
+let vysledokPrikladu =
+  prvaPremenna + druhaPremenna * (tretiaPremenna - stvrtaPremenna);
+vysledok = vysledokPrikladu;
+//alert(15 + 3 * (4 - 8 / 2));
+alert(vysledokPrikladu);
 alert();
-alert(prvaPremenna + druhaPremenna);
+//alert(15 + 3 * (4 - 8 / 2));
+let premennaPrva = 150.2 / 8;
+let premennaDruha = 18.3;
+let premennaTretia = 20.1;
+let premennaStvrta = 3;
+let rieseniePrikladu =
+  (premennaPrva * (premennaDruha - premennaTretia)) / premennaStvrta;
+vysledok = rieseniePrikladu;
+alert(rieseniePrikladu);
 alert();
-alert(vysledok);
