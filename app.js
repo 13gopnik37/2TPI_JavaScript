27ff479d68655b9d250b5fde10f3bd28aacd5161
Adam Miholek <Adam.Miholek@sosholic.sk>
2023-10-25 12:53:37 +0200

    alert

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-alert("prvy alert");
-let prvaPremenna = 3;
-const druhaPremenna = 7;
-prvaPremenna = 20;
-const meno = "adam";
-let vysledokScitania = prvaPremenna + druhaPremenna;
-
-vysledok(vysledokScitania);
-
+//alert("prvy alert");
 //let prvaPremenna = 3;
-//let druhaPremenna = 7;
+//const druhaPremenna = 7;
+//prvaPremenna = 20;
+//const meno = "adam";
+//let vysledokScitania = prvaPremenna + druhaPremenna;
+
+//vysledok(vysledokScitania);
+
+let prvaPremenna = 3;
+let druhaPremenna = 7;
+let vysledokScitania = prvaPremenna + druhaPremenna;
+vysledok = vysledokScitania;
+alert(vysledok);
