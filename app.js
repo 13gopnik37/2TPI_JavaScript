2f693bdbfd3f7b97ddb555769013005f34df6f7c
Adam Miholek <Adam.Miholek@sosholic.sk>
2023-11-08 12:57:49 +0100

    .

--- app.js
+++ app.js
@@ -18,32 +18,45 @@
 //alert();
 //alert(vysledok);
 
-let prvaPremenna = 15;
-let druhaPremenna = 3;
-let tretiaPremenna = 4;
-let stvrtaPremenna = 8 / 2;
-let vysledokPrikladu =
-  prvaPremenna + druhaPremenna * (tretiaPremenna - stvrtaPremenna);
-vysledok = vysledokPrikladu;
+//let prvaPremenna = 15;
+//let druhaPremenna = 3;
+//let tretiaPremenna = 4;
+//let stvrtaPremenna = 8 / 2;
+//let vysledokPrikladu =
+//prvaPremenna + druhaPremenna * (tretiaPremenna - stvrtaPremenna);
+//vysledok = vysledokPrikladu;
 //alert(15 + 3 * (4 - 8 / 2));
-alert(vysledokPrikladu);
-alert();
+//alert(vysledokPrikladu);
+//alert();
 //alert(15 + 3 * (4 - 8 / 2));
-let premennaPrva = 150.2 / 8;
-let premennaDruha = 18.3;
-let premennaTretia = 20.1;
-let premennaStvrta = 3;
-let rieseniePrikladu =
-  (premennaPrva * (premennaDruha - premennaTretia)) / premennaStvrta;
-vysledok = rieseniePrikladu;
-alert(rieseniePrikladu);
-alert();
-let PremennaPrva = 3;
-let PremennaDruha = 2;
-let PremennaTretia = 8;
-let PremennaStvrta = 31 / 8;
-let prikladuRiesenie =
-  PremennaPrva ** PremennaDruha + PremennaTretia - PremennaStvrta;
-vysledok = prikladuRiesenie;
-alert(prikladuRiesenie);
-alert();
+//let premennaPrva = 150.2 / 8;
+//let premennaDruha = 18.3;
+//let premennaTretia = 20.1;
+//let premennaStvrta = 3;
+//let rieseniePrikladu =
+//(premennaPrva * (premennaDruha - premennaTretia)) / premennaStvrta;
+//vysledok = rieseniePrikladu;
+//alert(rieseniePrikladu);
+//alert();
+//let PremennaPrva = 3;
+//let PremennaDruha = 2;
+//let PremennaTretia = 8;
+//let PremennaStvrta = 31 / 8;
+//let prikladuRiesenie =
+//PremennaPrva ** PremennaDruha + PremennaTretia - PremennaStvrta;
+//vysledok = prikladuRiesenie;
+//alert(prikladuRiesenie);
+//alert();
+
+// let MojVek = 16;
+// alert(MojVek);
+// alert();
+let meno = "Sulec ";
+let priezvisko = "Barkozi";
+let MojeMeno = meno + priezvisko;
+alert(MojeMeno);
+
+let MojaAdresa = "Hurbanova 39 908 48 Kopcany";
+alert(MojaAdresa);
+let AdresaSkoly = "Námestie sv.Martina 5 908 51 Holíč";
+alert(AdresaSkoly);
