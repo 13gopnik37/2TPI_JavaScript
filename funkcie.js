a660ea555847a50b8d24270c7c075c44a0f22900
Adam Miholek <Adam.Miholek@sosholic.sk>
2023-11-24 08:27:41 +0100

    22

---
+++ funkcie.js
@@ -0,0 +1,54 @@
+// mojaPrvaFunkcia();
+// function mojaPrvaFunkcia() {
+//   alert("toto je moja prva funckia");
+//   console.log("TOTO JE MOJA PRVA FUNCKIA!!!!!");
+// }
+// mojaPrvaFunkciaSParametrom("toto je moja prva funkcia s parametrom");
+// function mojaPrvaFunkciaSParametrom(lokalnaPremenna) {
+//   console.log(lokalnaPremenna);
+//}
+// console.log(a.indexOf("te"));
+// console.log(console);
+// console.log(a.toLowerCase());
+// console.log(a.toUpperCase());
+// console.log(a.substring(3, 7)); *
+// console.log(a.endsWith("3")); *
+// console.log(a.repeat(10));
+// console.log(a.replace("Ulica", "namestie")); *
+
+function funkcia() {
+  alert("tot je moja prva funkcia");
+  console.log("TOTO JE MOJA PRVA FUNCKIA!!!!");
+}
+mojaPrvaFunkciaSParametrom("toto je moja prva funkcia s parametrom");
+function mojaPrvaFunkciaSParametrom(lokalnaPremenna) {
+  console.log(lokalnaPremenna);
+}
+mojaToUpperCase("funkcia s malym");
+function mojaToUpperCase(vstupnyText) {
+  let upravenyText = vstupnyText.toUpperCase();
+  console.log(upravenyText);
+}
+mojaRepeat("ahoj ", 10);
+function mojaRepeat(k, cislo) {
+  let j = k.repeat(cislo);
+  console.log(j);
+}
+
+mojaSubString("Ahoj ja sa volam", 2, 5);
+function mojaSubString(t, cislica, druha) {
+  let b = t.substring(cislica, druha);
+  console.log(b);
+}
+
+mojaEndsWith("ahoj ja sa volam", "i");
+function mojaEndsWith(r, premenna) {
+  let o = r.endsWith(premenna);
+  console.log(o);
+}
+
+mojaReplace("ahoj ja sa volam", "ahoj", "cau");
+function mojaReplace(h, prvaPremenna, druhaPremenna) {
+  let n = h.replace(prvaPremenna, druhaPremenna);
+  console.log(n);
+}
